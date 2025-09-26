fdcb2c6f76706a8808a52ed58dc818160c01030b
Yao-Xinchen <yao29@illinois.edu>
2025-09-26 09:29:28 -0500

    fix pd param bug, and tune params

--- exts/bipedal_locomotion/bipedal_locomotion/tasks/locomotion/cfg/WF/limx_base_env_cfg.py
+++ exts/bipedal_locomotion/bipedal_locomotion/tasks/locomotion/cfg/WF/limx_base_env_cfg.py
@@ -269,22 +269,22 @@ class EventsCfg:
         params={
             "asset_cfg": SceneEntityCfg("robot", body_names=".*"),
             "static_friction_range": (0.4, 1.2),
-            "dynamic_friction_range": (0.7, 0.9),
+            "dynamic_friction_range": (0.2, 0.9),
             "restitution_range": (0.0, 1.0),
             "num_buckets": 48,
         },
     )
-    robot_joint_stiffness_and_damping = EventTerm(
-        func=mdp.randomize_actuator_gains,
-        mode="startup",
-        params={
-            "asset_cfg": SceneEntityCfg("robot", joint_names=".*"),
-            "stiffness_distribution_params": (32, 48),
-            "damping_distribution_params": (2.0, 3.0),
-            "operation": "abs",
-            "distribution": "uniform",
-        },
-    )
+    # robot_joint_stiffness_and_damping = EventTerm(
+    #     func=mdp.randomize_actuator_gains,
+    #     mode="startup",
+    #     params={
+    #         "asset_cfg": SceneEntityCfg("robot", joint_names=".*"),
+    #         "stiffness_distribution_params": (32, 48),
+    #         "damping_distribution_params": (2.0, 3.0),
+    #         "operation": "abs",
+    #         "distribution": "uniform",
+    #     },
+    # )
     robot_center_of_mass = EventTerm(
         func=mdp.randomize_rigid_body_coms,
         mode="startup",
@@ -323,17 +323,17 @@ class EventsCfg:
         },
     )
 
-    # randomize_actuator_gains = EventTerm(
-    #     func=mdp.randomize_actuator_gains,
-    #     mode="reset",
-    #     params={
-    #         "asset_cfg": SceneEntityCfg("robot", joint_names=".*"),
-    #         "stiffness_distribution_params": (0.5, 2.0),
-    #         "damping_distribution_params": (0.5, 2.0),
-    #         "operation": "scale",
-    #         "distribution": "log_uniform",
-    #     },
-    # )
+    randomize_actuator_gains = EventTerm(
+        func=mdp.randomize_actuator_gains,
+        mode="reset",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", joint_names=".*"),
+            "stiffness_distribution_params": (0.5, 2.0),
+            "damping_distribution_params": (0.5, 2.0),
+            "operation": "scale",
+            "distribution": "log_uniform",
+        },
+    )
 
     push_robot = EventTerm(
         func=mdp.apply_external_force_torque_stochastic,
--- exts/bipedal_locomotion/bipedal_locomotion/tasks/locomotion/mdp/curriculums.py
+++ exts/bipedal_locomotion/bipedal_locomotion/tasks/locomotion/mdp/curriculums.py
@@ -74,7 +74,7 @@ def pos_commands_ranges_level(
     env: ManagerBasedRLEnv,
     env_ids: Sequence[int],
     max_range: dict[str, tuple[float, float]],
-    update_interval: int = 50 * 24,
+    update_interval: int = 80 * 24,
     asset_cfg: SceneEntityCfg = SceneEntityCfg("robot"),
     command_name: str = "base_pose",
 ) -> torch.Tensor:
@@ -82,8 +82,8 @@ def pos_commands_ranges_level(
     command_cfg: mdp.UniformPoseCommandCfg = env.command_manager.get_term(command_name).cfg
     x = command_cfg.ranges.pos_x[1]
     if (env.common_step_counter + 1) % update_interval == 0:
-        x = command_cfg.ranges.pos_x[1] + 0.2
-        y = command_cfg.ranges.pos_y[1] + 0.2
+        x = command_cfg.ranges.pos_x[1] + 0.1
+        y = command_cfg.ranges.pos_y[1] + 0.1
         x = min(x, max_range["pos_x"][1])
         y = min(y, max_range["pos_y"][1])
         command_cfg.ranges.pos_x = (-x, x)
